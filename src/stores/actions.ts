76f81fc89f1394516480c53eeeefb2cae8b23214
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-08 13:30:27 -0600

    refactor: Update weather API endpoint in geolocation and useCounterStore actions

--- src/stores/actions.ts
+++ src/stores/actions.ts
@@ -4,13 +4,7 @@ export const useCounterStore = defineStore('counter', () => {
   async function geolocation(position: GeolocationPosition) {
     const latitude = position.coords.latitude
     const longitude = position.coords.longitude
-    return fetch('/api/geolocation', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ latitude, longitude })
-    })
+    return fetch(`https://wheater-api.fly.dev/api/weather/coordinates/${latitude}/${longitude}`)
       .then((response) => response.json())
       .then((data) => {
         return data
@@ -22,6 +16,7 @@ export const useCounterStore = defineStore('counter', () => {
     return fetch(`https://wheater-api.fly.dev/api/weather/${latitude}/${longitude}`)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data)
         return data
       })
       .catch((error) => console.error('Error al obtener los datos del clima:', error))
