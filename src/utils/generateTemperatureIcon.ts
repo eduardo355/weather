bbcde8f137c96d6766774634e2d1ffc18edef2ec
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-21 14:58:50 -0600

    feat: Add FogIcon and RainIcon components
    Import the FogIcon and RainIcon components in the generateTemperatureIcon utility function to handle the 'fog' and 'rain' temperature types.

--- src/utils/generateTemperatureIcon.ts
+++ src/utils/generateTemperatureIcon.ts
@@ -1,3 +1,5 @@
+import FogIcon from '@/components/icons/FogIcon.vue'
+import RainIcon from '@/components/icons/RainIcon.vue'
 import CloudyIcon from '@/components/icons/CloudyIcon.vue'
 import PartlyCloudyNight from '@/components/icons/PartlyCloudyNight.vue'
 import PartlyCloudyDayIcon from '@/components/icons/PartlyCloudyDayIcon.vue'
@@ -6,4 +8,6 @@ export const generateTemperatureIcon = (temperature: string) => {
   if (temperature === 'partly-cloudy-night') return PartlyCloudyNight
   if (temperature === 'partly-cloudy-day') return PartlyCloudyDayIcon
   if (temperature === 'cloudy') return CloudyIcon
+  if (temperature === 'rain') return RainIcon
+  if (temperature === 'fog') return FogIcon
 }
