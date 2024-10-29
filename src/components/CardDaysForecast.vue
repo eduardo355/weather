fdf8515f5ae0555b382ad4269c2427f246aca673
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-29 17:13:17 -0600

    refactor: Add CardDaysForecast component
    This commit adds the CardDaysForecast component, which displays a 15-day weather forecast. It includes the date, temperature range, weather icon, and precipitation probability for each day. The component uses utility functions to convert temperatures from Fahrenheit to Celsius and generate appropriate weather icons.

---
+++ src/components/CardDaysForecast.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="w-1/2 px-4 py-4">
+      <h2 class="text-2xl font-semibold">Pronóstico a 15 dias</h2>
+
+      <div
+        v-for="(item, index) in data"
+        class="grid grid-cols-4 place-content-center place-items-center items-center py-2"
+        :key="index"
+      >
+        <span>{{ item.datetime }}</span>
+        <span class="text-sky-500 text-2xl font-semibold"
+          >{{ fahrenheitToCelsius(item.tempmax) }}°/{{ fahrenheitToCelsius(item.tempmin) }}°</span
+        >
+        <component :is="generateTemperatureIcon(item.icon)" class="h-20" />
+        <div class="flex items-center space-x-2">
+          <component :is="generateTemperatureIcon('rain')" class="h-20" />
+          <span>{{ item.precipprob }}%</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { fahrenheitToCelsius } from '@/utils/fahrenheitToCelsius'
+import { generateTemperatureIcon } from '@/utils/generateTemperatureIcon'
+
+defineProps<{
+  data: any
+}>()
+</script>
