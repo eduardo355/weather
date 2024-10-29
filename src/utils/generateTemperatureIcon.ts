9e2c0346d9d2bbabdf27edf3479fb71fd3f2a155
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-28 18:20:19 -0600

    feat: Add ClearDay and ClearNight icons
    Add ClearDay.vue and ClearNight.vue components to display clear weather icons. These components are used to generate temperature icons based on the weather condition. The ClearDay icon represents a clear day, while the ClearNight icon represents a clear night. These icons are added to the existing list of temperature icons in the generateTemperatureIcon utility function.
    
    Refactor the generateTemperatureIcon utility function to include the new icons. The function now returns the ClearDay component when the temperature is 'clear-day' and the ClearNight component when the temperature is 'clear-night'.
    
    This commit adds the necessary code changes to introduce the ClearDay and ClearNight icons and update the generateTemperatureIcon utility function to support these icons.

--- src/utils/generateTemperatureIcon.ts
+++ src/utils/generateTemperatureIcon.ts
@@ -1,12 +1,16 @@
 import FogIcon from '@/components/icons/FogIcon.vue'
+import ClearDay from '@/components/icons/ClearDay.vue'
 import RainIcon from '@/components/icons/RainIcon.vue'
 import CloudyIcon from '@/components/icons/CloudyIcon.vue'
+import ClearNight from '@/components/icons/ClearNight.vue'
 import PartlyCloudyNight from '@/components/icons/PartlyCloudyNight.vue'
 import PartlyCloudyDayIcon from '@/components/icons/PartlyCloudyDayIcon.vue'
 
 export const generateTemperatureIcon = (temperature: string) => {
   if (temperature === 'partly-cloudy-night') return PartlyCloudyNight
   if (temperature === 'partly-cloudy-day') return PartlyCloudyDayIcon
+  if (temperature === 'clear-night') return ClearNight
+  if (temperature === 'clear-day') return ClearDay
   if (temperature === 'cloudy') return CloudyIcon
   if (temperature === 'rain') return RainIcon
   if (temperature === 'fog') return FogIcon
